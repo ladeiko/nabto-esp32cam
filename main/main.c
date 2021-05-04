7b89c5ccc2fb7a9194375670e38877352c865fed
Siarhei Ladzeika <sergey.ladeiko@bpmobile.com>
2021-05-04 17:27:56 +0300

    add support for multiple wifi candidates (you can pass ssid/password using specified separator)

--- main/main.c
+++ main/main.c
@@ -11,7 +11,6 @@
 #include "esp_camera.h"
 #include <driver/gpio.h>
 
-
 #include <esp_http_server.h>
 
 #include "demo_application.h"
@@ -100,6 +99,11 @@ const int CONNECTED_BIT = BIT0;
 
 #define CAM_XCLK_FREQ   20000000
 
+// SCAN OPTIONS
+#define DEFAULT_SCAN_LIST_SIZE      32
+#define CANDIDATE_SEPARATOR         '|'
+#define SCAN_INTERVAL               30 // in seconds
+#define MAX_CANDIDATES              64
 
 static camera_config_t camera_config = {
     .pin_pwdn  = CAM_PIN_PWDN,
@@ -434,6 +438,204 @@ void main_task(void *pvParameter)
 
 }
 
+static const char *TAG = "scan";
+
+static void print_cipher_type(int pairwise_cipher, int group_cipher)
+{
+    switch (pairwise_cipher) {
+    case WIFI_CIPHER_TYPE_NONE:
+        ESP_LOGI(TAG, "Pairwise Cipher \tWIFI_CIPHER_TYPE_NONE");
+        break;
+    case WIFI_CIPHER_TYPE_WEP40:
+        ESP_LOGI(TAG, "Pairwise Cipher \tWIFI_CIPHER_TYPE_WEP40");
+        break;
+    case WIFI_CIPHER_TYPE_WEP104:
+        ESP_LOGI(TAG, "Pairwise Cipher \tWIFI_CIPHER_TYPE_WEP104");
+        break;
+    case WIFI_CIPHER_TYPE_TKIP:
+        ESP_LOGI(TAG, "Pairwise Cipher \tWIFI_CIPHER_TYPE_TKIP");
+        break;
+    case WIFI_CIPHER_TYPE_CCMP:
+        ESP_LOGI(TAG, "Pairwise Cipher \tWIFI_CIPHER_TYPE_CCMP");
+        break;
+    case WIFI_CIPHER_TYPE_TKIP_CCMP:
+        ESP_LOGI(TAG, "Pairwise Cipher \tWIFI_CIPHER_TYPE_TKIP_CCMP");
+        break;
+    default:
+        ESP_LOGI(TAG, "Pairwise Cipher \tWIFI_CIPHER_TYPE_UNKNOWN");
+        break;
+    }
+
+    switch (group_cipher) {
+    case WIFI_CIPHER_TYPE_NONE:
+        ESP_LOGI(TAG, "Group Cipher \tWIFI_CIPHER_TYPE_NONE");
+        break;
+    case WIFI_CIPHER_TYPE_WEP40:
+        ESP_LOGI(TAG, "Group Cipher \tWIFI_CIPHER_TYPE_WEP40");
+        break;
+    case WIFI_CIPHER_TYPE_WEP104:
+        ESP_LOGI(TAG, "Group Cipher \tWIFI_CIPHER_TYPE_WEP104");
+        break;
+    case WIFI_CIPHER_TYPE_TKIP:
+        ESP_LOGI(TAG, "Group Cipher \tWIFI_CIPHER_TYPE_TKIP");
+        break;
+    case WIFI_CIPHER_TYPE_CCMP:
+        ESP_LOGI(TAG, "Group Cipher \tWIFI_CIPHER_TYPE_CCMP");
+        break;
+    case WIFI_CIPHER_TYPE_TKIP_CCMP:
+        ESP_LOGI(TAG, "Group Cipher \tWIFI_CIPHER_TYPE_TKIP_CCMP");
+        break;
+    default:
+        ESP_LOGI(TAG, "Group Cipher \tWIFI_CIPHER_TYPE_UNKNOWN");
+        break;
+    }
+}
+
+static void print_auth_mode(int authmode)
+{
+    switch (authmode) {
+    case WIFI_AUTH_OPEN:
+        ESP_LOGI(TAG, "Authmode \tWIFI_AUTH_OPEN");
+        break;
+    case WIFI_AUTH_WEP:
+        ESP_LOGI(TAG, "Authmode \tWIFI_AUTH_WEP");
+        break;
+    case WIFI_AUTH_WPA_PSK:
+        ESP_LOGI(TAG, "Authmode \tWIFI_AUTH_WPA_PSK");
+        break;
+    case WIFI_AUTH_WPA2_PSK:
+        ESP_LOGI(TAG, "Authmode \tWIFI_AUTH_WPA2_PSK");
+        break;
+    case WIFI_AUTH_WPA_WPA2_PSK:
+        ESP_LOGI(TAG, "Authmode \tWIFI_AUTH_WPA_WPA2_PSK");
+        break;
+    case WIFI_AUTH_WPA2_ENTERPRISE:
+        ESP_LOGI(TAG, "Authmode \tWIFI_AUTH_WPA2_ENTERPRISE");
+        break;
+    case WIFI_AUTH_WPA3_PSK:
+        ESP_LOGI(TAG, "Authmode \tWIFI_AUTH_WPA3_PSK");
+        break;
+    case WIFI_AUTH_WPA2_WPA3_PSK:
+        ESP_LOGI(TAG, "Authmode \tWIFI_AUTH_WPA2_WPA3_PSK");
+        break;
+    default:
+        ESP_LOGI(TAG, "Authmode \tWIFI_AUTH_UNKNOWN");
+        break;
+    }
+}
+
+typedef struct wifi_network_candidate {
+    uint8_t ssid[33];
+    uint8_t pass[64];
+} wifi_network_candidate_t;
+
+static wifi_network_candidate_t wifiCandidates[(MAX_CANDIDATES) + 1] = {0};
+
+static void setup_candidates()
+{
+    const uint8_t* ssid = (const uint8_t*)WIFI_SSID;
+    const uint8_t* pass = (const uint8_t*)WIFI_PASS;
+
+    size_t i = 0;
+    while (i < (sizeof(wifiCandidates) / sizeof(wifiCandidates[0])) - 1) {
+        const uint8_t* const nextSsid = (const uint8_t*)strchr((const char*)ssid, CANDIDATE_SEPARATOR);
+        const uint8_t* const nextPass = (const uint8_t*)strchr((const char*)pass, CANDIDATE_SEPARATOR);
+        
+        if (!nextSsid) {
+            if (strlen((const char*)ssid) > 0) {
+                strcpy((char*)wifiCandidates[i].ssid, (const char*)ssid);
+                strcpy((char*)wifiCandidates[i].pass, (const char*)pass);
+            }
+            break;
+        }
+
+        bool copied = false;
+
+        const size_t ssidLength = nextSsid - ssid;
+
+        if (ssidLength > 0) {
+            memcpy(wifiCandidates[i].ssid, ssid, ssidLength);
+            copied = true;
+        }
+
+        ssid = nextSsid + 1;
+
+        if (nextPass) {
+            if (copied) {
+                memcpy(wifiCandidates[i].ssid, ssid, nextPass - pass);
+            }
+            pass = nextPass + 1;
+        }
+        else {
+            if (copied) {
+                strcpy((char*)wifiCandidates[i].pass, (const char*)pass);
+            }
+        }
+
+        if (copied) {
+            ++i;
+        }
+    }
+}
+
+/* Sleep current thread for specified number of seconds */
+static void cross_sleep_sec(int sec) {
+    const TickType_t xDelay = (sec * 1000) / portTICK_PERIOD_MS;
+    vTaskDelay(xDelay);
+}
+
+/* Initialize Wi-Fi as sta and set scan method */
+static wifi_network_candidate_t wifi_scan(void)
+{
+    setup_candidates();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
+    assert(sta_netif);
+
+    ESP_ERROR_CHECK(esp_wifi_start());
+    esp_wifi_scan_start(NULL, true);
+
+    uint16_t number = DEFAULT_SCAN_LIST_SIZE;
+    const size_t ap_info_size = sizeof(wifi_ap_record_t) * DEFAULT_SCAN_LIST_SIZE;
+    wifi_ap_record_t* const ap_info = malloc(ap_info_size);
+
+    while (true) {
+        ESP_LOGI(TAG, "Scanning networks...\n");
+
+        uint16_t ap_count = 0;
+        memset(ap_info, 0, ap_info_size);
+
+        ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&number, ap_info));
+        ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&ap_count));
+
+        ESP_LOGI(TAG, "Total APs scanned = %u", ap_count);
+        for (int i = 0; (i < DEFAULT_SCAN_LIST_SIZE) && (i < ap_count); i++) {
+            ESP_LOGI(TAG, "SSID \t\t%s", ap_info[i].ssid);
+            ESP_LOGI(TAG, "RSSI \t\t%d", ap_info[i].rssi);
+            print_auth_mode(ap_info[i].authmode);
+            if (ap_info[i].authmode != WIFI_AUTH_WEP) {
+                print_cipher_type(ap_info[i].pairwise_cipher, ap_info[i].group_cipher);
+            }
+            ESP_LOGI(TAG, "Channel \t\t%d\n", ap_info[i].primary);
+            for (size_t n = 0; n < MAX_CANDIDATES; ++n) {
+                if (wifiCandidates[n].ssid[0] == 0) {
+                    break;
+                }
+                if (strcmp((const char*)ap_info[i].ssid, (const char*)wifiCandidates[n].ssid) == 0) {
+                    ESP_LOGI(TAG, "Candidate matches \t\t%s\n", ap_info[i].ssid);
+                    esp_wifi_stop();
+                    free(ap_info);
+                    return wifiCandidates[n];
+                }
+            }
+        }
+
+        ESP_LOGI(TAG, "Waiting for the next scan %d second(s)\n", SCAN_INTERVAL);
+        cross_sleep_sec(SCAN_INTERVAL);
+    }
+}
 
 /*
  * Main application entry
@@ -508,14 +710,20 @@ void app_main()
   ESP_ERROR_CHECK(esp_wifi_init(&wifi_init_config));
   ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
   ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+
+  const wifi_network_candidate_t network = wifi_scan();
   
   // configure the wifi connection and start the interface
   wifi_config_t wifi_config = {
-    .sta = {
-      .ssid = WIFI_SSID,
-      .password = WIFI_PASS,
-    },
+    // .sta = {
+    //   .ssid = network.ssid,
+    //   .password = network.pass,
+    // },
   };
+  
+  memcpy(wifi_config.sta.ssid, network.ssid, sizeof(network.ssid));
+  memcpy(wifi_config.sta.ssid, network.pass, sizeof(network.pass));
+
   ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
   ESP_ERROR_CHECK(esp_wifi_start());
   NABTO_LOG_INFO(("Connecting to %s\n", WIFI_SSID));
